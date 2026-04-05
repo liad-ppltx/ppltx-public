08dd903f940e235004e6ec4583d4e6b8af497053
guy malka <guymalka3@gmail.com>
2026-04-05 20:13:26 +0300

    update

--- validations/data_quality.sql
+++ validations/data_quality.sql
@@ -1,39 +1,61 @@
 SELECT
   eventName,
-  COUNT(*) AS total_occurrences,
+  COUNT(*) AS total_events,
 
-  -- ספירת פרמטרים של Session
-  COUNT(JSON_VALUE(params, '$.initialSpins')) AS has_initialSpins,
-  COUNT(JSON_VALUE(params, '$.initialCoins')) AS has_initialCoins,
-  COUNT(JSON_VALUE(params, '$.consent')) AS has_consent,
-  COUNT(JSON_VALUE(params, '$.totalSessionTime')) AS has_totalSessionTime,
+  -- 1. session_start & session_end
+  COUNT(JSON_VALUE(params, '$.initialSpins')) AS p_initialSpins,
+  COUNT(JSON_VALUE(params, '$.initialCoins')) AS p_initialCoins,
+  COUNT(JSON_VALUE(params, '$.consent')) AS p_consent,
+  COUNT(JSON_VALUE(params, '$.totalSessionTime')) AS p_totalSessionTime,
+  COUNT(JSON_VALUE(params, '$.finalLevel')) AS p_finalLevel,
+  COUNT(JSON_VALUE(params, '$.finalCoins')) AS p_finalCoins,
 
-  -- ספירת פרמטרים של Spin
-  COUNT(JSON_VALUE(params, '$.spinIndex')) AS has_spinIndex,
-  COUNT(JSON_VALUE(params, '$.payoutCoins')) AS has_payoutCoins,
-  COUNT(JSON_VALUE(params, '$.actionTriggered')) AS has_actionTriggered,
-  COUNT(JSON_VALUE(params, '$.reactionTimeMs')) AS has_reactionTimeMs,
+  -- 2. building_upgrade
+  COUNT(JSON_VALUE(params, '$.status')) AS p_upgradeStatus,
+  COUNT(JSON_VALUE(params, '$.building')) AS p_buildingId,
+  COUNT(JSON_VALUE(params, '$.levelAfter')) AS p_levelAfter,
+  COUNT(JSON_VALUE(params, '$.cost')) AS p_upgradeCost,
+  COUNT(JSON_VALUE(params, '$.isFree')) AS p_isFree,
+  COUNT(JSON_VALUE(params, '$.reason')) AS p_upgradeReason,
+  COUNT(JSON_VALUE(params, '$.cannonIndex')) AS p_cannonIndex,
+  COUNT(JSON_VALUE(params, '$.castleStageAfter')) AS p_castleStageAfter,
 
-  -- ספירת פרמטרים של Upgrades
-  COUNT(JSON_VALUE(params, '$.building')) AS has_building,
-  COUNT(JSON_VALUE(params, '$.status')) AS has_upgradeStatus,
-  COUNT(JSON_VALUE(params, '$.cost')) AS has_upgradeCost,
+  -- 3. spin
+  COUNT(JSON_VALUE(params, '$.spinIndex')) AS p_spinIndex,
+  COUNT(JSON_VALUE(params, '$.energyBefore')) AS p_energyBefore,
+  COUNT(JSON_VALUE(params, '$.energyAfter')) AS p_energyAfter,
+  COUNT(JSON_QUERY(params, '$.symbols')) AS p_symbols_array,
+  COUNT(JSON_VALUE(params, '$.payoutCoins')) AS p_payoutCoins,
+  COUNT(JSON_VALUE(params, '$.actionTriggered')) AS p_actionTriggered,
+  COUNT(JSON_VALUE(params, '$.reactionTimeMs')) AS p_reactionTimeMs,
 
-  -- ספירת פרמטרים של Attack/Raid
-  COUNT(JSON_VALUE(params, '$.targetName')) AS has_attackTarget,
-  COUNT(JSON_VALUE(params, '$.result')) AS has_attackResult,
-  COUNT(JSON_VALUE(params, '$.totalStolen')) AS has_totalStolen,
-  COUNT(JSON_VALUE(params, '$.perfectRaid')) AS has_perfectRaid,
+  -- 4. attack & raid
+  COUNT(JSON_VALUE(params, '$.targetName')) AS p_attackTarget,
+  COUNT(JSON_VALUE(params, '$.result')) AS p_attackResult,
+  COUNT(JSON_VALUE(params, '$.reward')) AS p_reward,
+  COUNT(JSON_VALUE(params, '$.rivalName')) AS p_raidRival,
+  COUNT(JSON_VALUE(params, '$.totalStolen')) AS p_totalStolen,
+  COUNT(JSON_VALUE(params, '$.perfectRaid')) AS p_perfectRaid,
 
-  -- ספירת פרמטרים של התקדמות
-  COUNT(JSON_VALUE(params, '$.newLevel')) AS has_newLevel,
-  COUNT(JSON_VALUE(params, '$.villageId')) AS has_villageId,
-  COUNT(JSON_VALUE(params, '$.phase')) AS has_tutorialPhase,
+  -- 5. energy_update
+  COUNT(JSON_VALUE(params, '$.kind')) AS p_energyKind,
+  COUNT(JSON_VALUE(params, '$.spinsAfter')) AS p_spinsAfterUpdate,
+  COUNT(JSON_VALUE(params, '$.nextRefillTime')) AS p_nextRefillTime,
 
-  -- ספירת פרמטרים של Audit
-  COUNT(JSON_VALUE(params, '$.hasEconomy')) AS has_audit_economy,
-  COUNT(JSON_VALUE(params, '$.hasLogger')) AS has_audit_logger
+  -- 6. tutorial & progress
+  COUNT(JSON_VALUE(params, '$.phase')) AS p_tutorialPhase,
+  COUNT(JSON_VALUE(params, '$.stepNumber')) AS p_stepNumber,
+  COUNT(JSON_VALUE(params, '$.newLevel')) AS p_newLevel,
+  COUNT(JSON_VALUE(params, '$.villageId')) AS p_villageId,
+  COUNT(JSON_VALUE(params, '$.timeSpent')) AS p_timeSpent,
+
+  -- 7. audit_stress_test
+  COUNT(JSON_VALUE(params, '$.hasEconomy')) AS p_audit_economy,
+  COUNT(JSON_VALUE(params, '$.hasXpSettings')) AS p_audit_xp,
+  COUNT(JSON_VALUE(params, '$.hasLogger')) AS p_audit_logger,
+  COUNT(JSON_VALUE(params, '$.hasRunSpin')) AS p_audit_runSpin
 
 FROM `ppltx-project-dev.playpltx.v_staging`
+WHERE version = '1.0.4'
 GROUP BY eventName
-ORDER BY total_occurrences DESC
+ORDER BY total_events DESC
---
+++ validations/readme.md
@@ -0,0 +1,3 @@
+## User ID
+Every time I refresh or reset the game as long as I don't clear the local memory it will be the same user ID.
+
